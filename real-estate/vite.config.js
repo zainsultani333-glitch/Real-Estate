f38e6a39cb17853cedec7ea8035d19b4818cda5c
zainsultani333-glitch <zainsultani333@gmail.com>
2025-09-13 15:55:26 +0500

    new

--- real-estate/vite.config.js
+++ real-estate/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE.PATH || "/Real-Estate"
+  base: process.env.VITE_BASE_PATH || "/Real-Estate"
 })
